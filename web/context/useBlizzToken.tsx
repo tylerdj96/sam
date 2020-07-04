a5fd11d15c0097695c51c0728e8dea109eeeabe2
tylerdj96 <tjblizz0517@gmail.com>
2020-07-04 00:48:55 -0500

    adding note

--- web/context/useBlizzToken.tsx
+++ web/context/useBlizzToken.tsx
@@ -13,6 +13,7 @@ const useBlizzToken = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [accessToken, setAccessToken] = useState<string>();
 
+  //TOKEN is not properly exchanged see https://github.com/expo/expo/issues/8300
   const discovery = {
     authorizationEndpoint: "https://us.battle.net/oauth/authorize",
     tokenEndpoint: "https://us.battle.net/oauth/token",
@@ -49,40 +50,7 @@ const useBlizzToken = () => {
     discovery
   );
 
-  // const [request, response, promptAsync]:  = AuthSession.useAuthRequest(
-  //   {
-  //     clientId: config.client_id,
-  //     clientSecret: "u0BVI5CiExd6JZ7FeUxO0K77N5tu1ib6",
-  //     scopes: ["wow.profile"],
-  //     // For usage in managed apps using the proxy
-  //     redirectUri: AuthSession.makeRedirectUri({
-  //       // For usage in bare and standalone
-  //       // Use your FBID here. The path MUST be `authorize`.
-  //       // native: `fb${FB_APP_ID}://authorize`,
-  //       useProxy,
-  //       native: "exp://",
-  //     }),
-  //     // NOTICE: Please do not actually request the token on the client (see:
-  //     // response_type=token in the authUrl), it is not secure. Request a code
-  //     // instead, and use this flow:
-  //     // https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow/#confirm
-  //     // The code here is simplified for the sake of demonstration. If you are
-  //     // just prototyping then you don't need to concern yourself with this and
-  //     // can copy this example, but be aware that this is not safe in production.
-  //     responseType: AuthSession.ResponseType.Token,
-  //   },
-  //   discovery
-  // );
-
-  // Request
-
   useEffect(() => {
-    // const loadToken = async () => {
-    //   setLoading(true);
-    //   const result = await getAccessToken();
-    //   setAccessToken(result);
-    //   setLoading(false);
-    // };
     const loadToken = async () => {
       console.log(request);
       console.log(response);
